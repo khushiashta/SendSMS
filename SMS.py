815c5b9c2de996fc45e084caea3d89bd5f925221
Khushi Ashta <khushiashta@192.168.1.6>
2023-07-25 23:31:32 +0530

    hidden sensitive information

--- SMS.py
+++ SMS.py
@@ -2,16 +2,16 @@ import os
 
 from twilio.rest import Client
 
-account_sid = 'AC79dbf2e53290d2423b9e7faaae18e0bf'
-auth_token = '25a149a332aa2fd32cf773a2e4093988'
+account_sid = '[auth_account]'
+auth_token = '[auth_token]'
 client = Client(account_sid, auth_token)
 
 
 message = client.messages \
     .create(
         body='This SMS is sent by python code',
-        from_='+12295455397',
-        to='+916239619772'
+        from_='Number_By_Twilio',
+        to='Your_Phone_Number'
         )  
 
 
